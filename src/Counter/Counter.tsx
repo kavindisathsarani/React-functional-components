31608cd7b4b9a763b1db3d382e7fc3e6adcc5ba9
kavindisathsarani <dksmanimendra0@gmail.com>
2025-05-10 15:32:45 +0530

    first commit

---
+++ src/Counter/Counter.tsx
@@ -0,0 +1,42 @@
+import './Counter.css'
+import {useEffect, useState} from "react";
+
+export function Counter(props: any) {
+
+    const [count,setCount]=useState(0);
+
+    useEffect(() => {
+        alert("componentDidMount: " +
+            "Component has been mounted! " +
+            "Props: " + props.data);
+
+        return () => {
+            alert("componentWillUnmount: " +
+                "Component is being removed");
+        }
+    },[]); //Run Only Once.deps=Dependencies
+
+    useEffect(() => {
+        alert("componentDidUpdate: "+
+        "count has been updated")
+    }, [count]);
+
+    const increment =()=>{
+        setCount((prevCount) => prevCount+1)
+    }
+
+    const decrement =()=>{
+        setCount((prevCount) => prevCount-1)
+    }
+
+    return (
+        <div className="counter">
+            <h1>React Counter(Functional Components)</h1>
+            <h2>Count:{count}</h2>
+            <div>
+                <button className="button" onClick={increment}>+</button>
+                <button className="button" onClick={decrement}>-</button>
+            </div>
+        </div>
+    );
+}
